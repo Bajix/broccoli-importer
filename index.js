d796b448b3806093bffc194e3edcf2fa1205166c
Bajix <Thomas@bajix.com>
2015-05-25 10:56:33 -0700

    Expose API for alternatively exporting assets from modules using relative funnels

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/importer').loadBrocfile;
+var Importer = require('./lib/importer');
+
+module.exports = function() {
+  if (arguments.length === 2) {
+    return Importer.createTree.apply(this, arguments);
+  }
+  return Importer.loadBrocfile.apply(this, arguments);
+}
