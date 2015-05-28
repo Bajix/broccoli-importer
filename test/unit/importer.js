6f46fe3a08f8abc870657f7d532334ee9b92682b
Bajix <Thomas@bajix.com>
2015-05-27 17:15:34 -0700

    Make tree path mapping dry

--- test/unit/importer.js
+++ test/unit/importer.js
@@ -1,12 +1,6 @@
 var Importer = require('../../lib/importer');
 
 describe('Broccoli Importer', function() {
-  it('Resolves paths', function() {
-    var brocfile = Importer.resolveBrocfile('sub-tree');
-
-    assert.match(brocfile, /Brocfile.js$/);
-  });
-
   it('Makes paths relative', function() {
     var tree = Importer.loadTree('sub-tree');
     assert.equal(tree, 'lib/sub-tree/assets');
